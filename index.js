932fb52660c260b4fbc720f49f11feed09c3937f
TawsifHossain007 <tawsifhossain18.o@gmail.com>
2025-12-09 23:29:59 +0600

    Added Delete

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 3000
-
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 3000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vtqh62q.mongodb.net/?appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,43 +17,47 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const db = client.db('Assignment-11')
-    const issueCollection = db.collection('issues');
-
+    const db = client.db("Assignment-11");
+    const issueCollection = db.collection("issues");
 
     //Issues
-    app.get('/issues',async(req,res)=>{
+    app.get("/issues", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
+      if (email) {
+        query.reporterEmail = email;
+      }
+      const cursor = issueCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        const query = {}
-        const {email} = req.query
-        if(email){
-            query.reporterEmail = email
-        }
-        const cursor = issueCollection.find(query)
-        const result = await cursor.toArray()
+    app.post("/issues", async (req, res) => {
+      const issues = req.body;
+      const result = await issueCollection.insertOne(issues);
+      res.send(result);
+    });
+
+    app.delete("/issues/:id", async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await issueCollection.deleteOne(query)
         res.send(result)
     })
-
-    app.post('/issues',async(req,res)=>{
-        const issues = req.body
-        const result = await issueCollection.insertOne(issues)
-        res.send(result)
-    })
-
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -63,10 +65,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
